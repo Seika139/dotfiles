817e01f7c2eb021c8ed151fcea4e329f843d0f3e
Seika139 <tcoanzu@gmail.com>
2022-01-18 19:13:40 +0900

    :recycle: winでもmacでも正しく動くように修正

--- bash/public/04_prompt.bash
+++ bash/public/04_prompt.bash
@@ -6,12 +6,15 @@
 # ref : https://qiita.com/zaburo/items/9194cd9eb841dea897a0
 # ref : https://qiita.com/hf7777hi/items/7585c3aa5423a44eef35
 
-# TODO : windowsでも大丈夫か確認
-
 USER='\[\e[40;92m\]\u@\h'
 TIME='\[\e[95m\]\t'
 DIR='\[\e[96m\]\w\[\e[49m\]'
-GIT="\[\e[1;32m\]$(__git_ps1 '(%s)')"
+if executable __git_ps1; then
+    GIT='\[\e[1;32m\]`__git_ps1 "(%s)"`'
+else
+    GIT=""
+    warn "__git_ps1 がありません。git-prompt.shが読み込まれていないようです。"
+fi
 LAST='\[\e[0m\]\n\$ '
 export PS1="${USER} ${TIME} ${DIR} ${GIT} ${LAST}"
 unset USER TIME DIR GIT LAST
