b07efa1c45fabea7180eebaa2e96e3ec6b2e1764
Seika139 <tcoanzu@gmail.com>
2022-01-18 19:34:08 +0900

    :recycle: git-promptをwinでも探すように追加

--- bash/public/03_git.bash
+++ bash/public/03_git.bash
@@ -4,24 +4,24 @@
 # ref : https://qiita.com/NorsteinBekkler/items/a0622ee6a39d08d61b72
 # ref : https://smootech.hatenablog.com/entry/2017/02/23/102531
 
-# TODO : windowsに対応
+# windowsのGitBashにも対応した
 
-files=(
-    "git-prompt.sh"
-    "git-completion.bash"
-)
 if executable brew; then
-    for file in "${files[@]}"; do
+    for file in "git-prompt.sh" "git-completion.bash"; do
         full_path="$(brew --prefix)/etc/bash_completion.d/${file}"
-        if [ -f "${full_path}" ]; then
+        if [ -e "${full_path}" ]; then
             source "${full_path}"
         else
-            warn "${full_path} が在りません"
+            warn "${full_path} が存在しません"
         fi
     done
     unset file full_path
+elif is_msys; then
+    full_path= "/c/Program\ Files/Git/etc/profile.d/git-prompt.sh"
+    if [ ! -e ${full_path} ]; then
+        warn "${full_path} が存在しません(違う場所にある可能性もあります)"
+    fi
 fi
-unset files
 
 # Gitブランチの状況を*+%で表示
 GIT_PS1_SHOWDIRTYSTATE=true
