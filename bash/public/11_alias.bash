312ca2e5a927a983d424f449422231058fd11c60
Seika139 <tcoanzu@gmail.com>
2022-01-25 02:59:21 +0900

    :sparkles: Add less_m

--- bash/public/11_alias.bash
+++ bash/public/11_alias.bash
@@ -3,6 +3,11 @@
 # lessのオプション設定 SEE : https://qiita.com/delphinus/items/b04752bb5b64e6cc4ea9
 export LESS="-g -i -M -R -S -W"
 
+function less_m() {
+    # windows で ^M がたくさん表示される時につかう
+    sed s/^M//g $1 | less $LESS
+}
+
 # lsにデフォで色をつける
 alias ls='ls -GF'
 alias ll="ls -al"
