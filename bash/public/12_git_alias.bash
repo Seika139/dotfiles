3d4984a492e7f2b2eb1fc4cff0cc7c899f3acc0b
Seika139 <tcoanzu@gmail.com>
2023-03-09 20:55:53 +0900

    新しいエイリアスを作成

--- bash/public/12_git_alias.bash
+++ bash/public/12_git_alias.bash
@@ -22,22 +22,24 @@ alias ga='git add'
 # git commit
 alias gc='git commit'
 
+PRETTY_FORMAT="%C(Yellow)%h %C(Magenta)%cd %C(Cyan)[%cn] %C(Reset)%s %C(Red)%d"
+
 # git log … 飾り付けて表示
-GL='git log --date=format-local:"%Y/%m/%d %H:%M:%S" --pretty=format:"%C(Yellow)%h %C(Magenta)%cd %C(Cyan)[%cn] %C(Reset)%s %C(Red)%d"'
+GL="git log --date=format-local:\"%Y/%m/%d %H:%M:%S\" --pretty=format:\"${PRETTY_FORMAT}\""
 alias gl="echo_yellow ${GL}; ${GL}"
 unset GL
 
 # git log … グラフ表示
-GLR='git log --date=format-local:"%Y/%m/%d %H:%M:%S" --pretty=format:"%C(Yellow)%h %C(Magenta)%cd %C(Cyan)[%cn] %C(Reset)%s %C(Red)%d" --graph'
+GLR="git log --date=format-local:\"%Y/%m/%d %H:%M:%S\" --pretty=format:\"${PRETTY_FORMAT}\" --graph"
 alias glr="echo_yellow ${GLR}; ${GLR}"
 unset GLR
 
 # git log … 修正ライン数が分かる
-GLL='git log --date=format-local:"%Y/%m/%d %H:%M:%S" --pretty=format:"%C(Yellow)%h %C(Magenta)%cd %C(Cyan)[%cn] %C(Reset)%s %C(Red)%d" --numstat'
+GLL="git log --date=format-local:\"%Y/%m/%d %H:%M:%S\" --pretty=format:\"${PRETTY_FORMAT}\" --numstat"
 alias gll="echo_yellow ${GLL}; ${GLL}"
 unset GLL
 
-alias gd='git diff'
+alias gd='git diff --src-prefix="BEFORE/" --dst-prefix=" AFTER/"'
 
 alias gr='git remote'
 alias grp='git remote prune origin'
@@ -60,6 +62,62 @@ function gmsg() {
 
 function hlp_git() {
     less_color ${DOTPATH}/docs/git.txt
+}
+
+# 自分がコミットした差分だけを確認したい
+function gdd() {
+    if [[ $1 == '--help' ]]; then
+        less <<EOS
+usage: gdd author commit1 [commit2] [option]
+
+commit1 & commit2 : Commit object. Not only hash but also branch name and tags are supported.
+option (optional) : Options for 'git diff' are acceptable, such as --stat, -w
+
+commit1 と commit2 の間で author が作成した差分を一覧表示する
+commit2 がない場合は commit1 と HEAD を比較する
+author を - にすると author で絞り込まない
+
+おすすめオプション
+--stat           : 変更量をファイル単位で確認
+-w --color-words : 改行コードや空白を無視しつつ、単語単位で差分を表示する
+EOS
+        return 0
+    fi
+
+    if [[ $# -lt 2 ]]; then
+        echo_yellow 'このエイリアスは最低2つの引数を必要とします'
+        echo_yellow "See : gdd --help"
+        return 1
+    fi
+
+    if [[ "$3" == -* ]]; then
+        local commit_after="@"
+        local options="${@:3}"
+    else
+        local commit_after="$3"
+        local options="${@:4}"
+    fi
+
+    # 引数が適切なコミットを指していない場合を弾く
+    for arg in $2 $commit_after; do
+        if [[ $(git show $arg | wc -l) -eq 0 ]]; then
+            echo
+            echo_red -n '不適切なコミット '
+            echo_yellow "$arg"
+            return 1
+        fi
+    done
+
+    if [[ "$1" == - ]]; then
+        # $1 を - にすると author で絞り込まない
+        git log --pretty=format:"%H" --no-merges $2..$commit_after |
+            xargs -n1 git --no-pager diff --name-only | sort -u |
+            xargs git diff --src-prefix="BEFORE/" --dst-prefix=" AFTER/" $options $2..$commit_after --
+    else
+        git log --pretty=format:"%H" --no-merges --author="$1" $2..$commit_after |
+            xargs -n1 git --no-pager diff --name-only | sort -u |
+            xargs git diff --src-prefix="BEFORE/" --dst-prefix=" AFTER/" $options $2..$commit_after --
+    fi
 }
 
 function tags_from_commit() {
@@ -138,7 +196,7 @@ EOS
         echo $(commit_with_tags $descendant)
         echo
         echo_rgb 180 255 180 "git log ${ancestor}..${descendant} ${@:3}"
-        git log --date=format-local:"%Y/%m/%d %H:%M:%S" --pretty=format:"%C(Yellow)%h %C(Magenta)%cd %C(Reset)%s %C(Cyan)[%cn]%C(Red)%d" "${ancestor}".."${descendant}" "${@:3}"
+        git log --date=format-local:"%Y/%m/%d %H:%M:%S" --pretty=format:"${PRETTY_FORMAT}" "${ancestor}".."${descendant}" "${@:3}"
 
     else
         # 両者のどちらとも同一でない祖先が存在する場合
@@ -158,8 +216,8 @@ EOS
         echo_rgb 180 255 180 "git log ${ancestor}..${commit_id_a} ${@:3}"
         echo_rgb 180 255 180 "git log ${ancestor}..${commit_id_b} ${@:3}"
 
-        git log --date=format-local:"%Y/%m/%d %H:%M:%S" --pretty=format:"%C(Yellow)%h %C(Magenta)%cd %C(Reset)%s %C(Cyan)[%cn]%C(Red)%d" "${ancestor}".."${commit_id_a}" "${@:3}"
-        git log --date=format-local:"%Y/%m/%d %H:%M:%S" --pretty=format:"%C(Yellow)%h %C(Magenta)%cd %C(Reset)%s %C(Cyan)[%cn]%C(Red)%d" "${ancestor}".."${commit_id_b}" "${@:3}"
+        git log --date=format-local:"%Y/%m/%d %H:%M:%S" --pretty=format:"${PRETTY_FORMAT}" "${ancestor}".."${commit_id_a}" "${@:3}"
+        git log --date=format-local:"%Y/%m/%d %H:%M:%S" --pretty=format:"${PRETTY_FORMAT}" "${ancestor}".."${commit_id_b}" "${@:3}"
 
     fi
 }
