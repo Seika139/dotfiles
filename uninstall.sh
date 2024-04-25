d10b8c73d920e7c6ea48b935eb5a20688e7390ab
Seika139 <tcoanzu@gmail.com>
2024-04-25 21:05:58 +0900

    uninstall.sh を改善した

--- uninstall.sh
+++ uninstall.sh
@@ -3,15 +3,23 @@
 # dotfileで作成したシンボリックリンクと ~/dotfiles を削除する
 
 source bash/public/01_util.bash
+
+if [[ $BASH_SOURCE == "$0" ]]; then
+    warn "このコマンドは source uninstall.sh で実行してください"
+    exit
+fi
+
 echo_yellow 'dotfiles およびホームディレクトリに作成したシンボリックリンクをすべて削除します'
 read -p "$(echo_yellow 'よろしいですか？ [y/N]: ')" ANS1
 if [[ $ANS1 != [yY] ]]; then
+    info "アンインストールをキャンセルしました"
     return 0
 fi
 unset ANS1
 echo_orange 'GitHubにあげていないファイルに関しては二度と復元できません'
 read -p "$(echo_orange 'それでもよろしいですか？ [y/N]: ')" ANS2
 if [[ $ANS2 != [yY] ]]; then
+    info "アンインストールをキャンセルしました"
     return 0
 fi
 unset ANS2
