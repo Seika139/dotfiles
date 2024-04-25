1209c0d7167f64b349ff7b2fc02e3db47c73cd99
Seika139 <tcoanzu@gmail.com>
2024-04-25 13:43:24 +0900

    ltsv_to_json を追加した

---
+++ bash/public/41_ltsv_to_json.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# LTSV を JSON に変換する
+ltsv_to_json() {
+    echo '['
+    local first_line=true
+    while IFS=$'\n' read -ra line; do
+        local first=true
+        if [ $first_line == false ]; then
+            echo ','
+        fi
+        echo '{'
+        IFS=$'\t' read -ra pairs <<<"$line"
+        for pair in "${pairs[@]}"; do
+            IFS=":" read key value <<<"$pair"
+            if [[ $value == "{"* ]]; then
+                if [ $first == true ]; then
+                    echo -n "  \"$key\": $value"
+                    first=false
+                else
+                    echo ","
+                    echo -n "  \"$key\": $value"
+                fi
+            else
+                if [ $first == true ]; then
+                    echo -n "  \"$key\": \"$value\""
+                    first=false
+                else
+                    echo ","
+                    echo -n "  \"$key\": \"$value\""
+                fi
+            fi
+        done
+        first_line=false
+        echo
+        echo '}'
+    done
+    echo
+    echo ']'
+}
+
+# 使い方
+# ltsv_json "input" > output
+# ltsv_json < file > output
