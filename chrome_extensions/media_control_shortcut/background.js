414fb991d1052233a3527cf612958ab23f07cd0b
Seika139 <tcoanzu@gmail.com>
2024-07-11 23:42:30 +0900

    メディアコントロールのChrome拡張 v6

--- chrome_extensions/media_control_shortcut/background.js
+++ chrome_extensions/media_control_shortcut/background.js
@@ -17,7 +17,16 @@ function toggleMediaPlayback() {
         mediaElements.forEach(media => {
             if (media.paused) {
                 console.log('Playing media:', media);
-                media.play().catch(error => console.error('Error playing media:', error));
+                media.play().catch(error => {
+                    console.error('Error playing media:', error);
+                    // Chromeには自動再生ポリシーがあり
+                    // 特定の条件下では自動再生がブロックされることがある
+                    // これを回避するために、ユーザー操作をシミュレートする方法を試す
+                    media.muted = true;
+                    media.play().then(() => {
+                        media.muted = false;
+                    }).catch(error => console.error('Error playing muted media:', error));
+                });
             } else {
                 console.log('Pausing media:', media);
                 media.pause();
