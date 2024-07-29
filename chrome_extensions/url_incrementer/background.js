538078a66124ed9d9c47b105788d36d7f437a863
Seika139 <tcoanzu@gmail.com>
2024-07-29 23:47:05 +0900

    #数字 の URL も操作できるようにした

--- chrome_extensions/url_incrementer/background.js
+++ chrome_extensions/url_incrementer/background.js
@@ -5,30 +5,45 @@ chrome.commands.onCommand.addListener((command) => {
             let tab = tabs[0];
             let url = new URL(tab.url);
             let pathParts = url.pathname.split('/');
+            let increase = (command === "increment");
 
-            let found = false;
-            for (let i = pathParts.length - 1; i >= 0; i--) {
-                let match = pathParts[i].match(/(\d+)(\.\w+)?$/);
+            // Function to handle increment/decrement
+            function updateNumber(match) {
+                let number = parseInt(match[1], 10);
+                let extension = match[2] || '';
+                if (increase) {
+                    number++;
+                } else {
+                    number--;
+                }
+                return number + extension;
+            }
+
+            // Search and update the pathname part
+            let updated = pathParts.some((part, index) => {
+                let match = part.match(/(\d+)(\.\w+)?$/);
                 if (match) {
-                    let number = parseInt(match[1], 10);
-                    let extension = match[2] || '';
-                    if (command === "increment") {
-                        number++;
-                    } else if (command === "decrement") {
-                        number--;
-                    }
-                    pathParts[i] = number + extension;
-                    found = true;
-                    break;
+                    pathParts[index] = updateNumber(match);
+                    url.pathname = pathParts.join('/');
+                    return true;
+                }
+                return false;
+            });
+
+            // If pathname part is not found, search and update the hash part
+            if (!updated) {
+                let hashMatch = url.hash.match(/#(\d+)$/);
+                if (hashMatch) {
+                    url.hash = `#${updateNumber(hashMatch)}`;
+                    updated = true;
                 }
             }
 
-            if (found) {
-                url.pathname = pathParts.join('/');
+            if (updated) {
                 console.log(`New URL: ${url.toString()}`);
                 chrome.tabs.update(tab.id, { url: url.toString() });
             } else {
-                console.log('No numeric part found in the URL path.');
+                console.log('No numeric part found in the URL path or hash.');
             }
         });
     }
