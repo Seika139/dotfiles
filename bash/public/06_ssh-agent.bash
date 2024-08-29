ce65dc5c559a0cca2be1a410b6dc8f5ce2649d44
Seika139 <tcoanzu@gmail.com>
2024-08-29 21:26:10 +0900

    ssh_agent.bash を更新した（パスキー対応）

--- bash/public/06_ssh-agent.bash
+++ bash/public/06_ssh-agent.bash
@@ -10,13 +10,31 @@
 SSH_ENV="${HOME}/.ssh/environment"
 SSHAGENT=/usr/bin/ssh-agent
 SSHAGENTARGS="-s"
+PF_FILE="${HOME}/.ssh/pass_phrase.txt"
 
 function start_agent {
     echo_yellow '新しい SSH Agent をインストールします'
-    ${SSHAGENT} | sed 's/^echo/#echo/' >"${SSH_ENV}"
+    if ! ${SSHAGENT} ${SSHAGENTARGS} | sed 's/^echo/#echo/' >"${SSH_ENV}"; then
+        echo "Error: Failed to start ssh-agent"
+        return 1
+    fi
     chmod 600 "${SSH_ENV}"
+    # shellcheck source=/dev/null
     . "${SSH_ENV}" >/dev/null
-    ssh-add # 秘密鍵のパスフレーズを入力させる
+
+    if [ -f "${PF_FILE}" ]; then
+        # パスフレーズのファイルがあるならクリップボードに登録する
+        if is_osx; then
+            pbcopy <"${PF_FILE}" # macOSの場合はpbcopyを使用
+        else
+            clip <"${PF_FILE}" # 他のOSの場合はclipを使用
+        fi
+    fi
+
+    if ! ssh-add; then
+        echo "Error: Failed to add SSH key"
+        return 1
+    fi
 }
 
 # mac では ssh-agent が自動的に立ち上がるので ssh-agent の面倒は見ない
@@ -24,13 +42,14 @@ if is_osx; then
     :
 elif [ -f "${SSH_ENV}" ]; then
     # Source SSH settings, if applicable
+    # shellcheck source=/dev/null
     . "${SSH_ENV}" >/dev/null
 
     # SSH_AGENT_PID がない場合は start_agentを実行する
     # || の前が true だと後ろは実行されないことを利用している
     # ref : http://okazu.air-nifty.com/blog/2010/04/bash-f628.html
 
-    if [ -z "${SSH_AGENT_PID}" ] || ! ps -p ${SSH_AGENT_PID} >/dev/null 2>&1; then
+    if [ -z "${SSH_AGENT_PID}" ] || ! ps -p "${SSH_AGENT_PID}" >/dev/null 2>&1; then
         echo -n 'SSH_AGENT_PID が未設定のため、'
         start_agent
     fi
@@ -43,6 +62,8 @@ elif [ -f "${SSH_ENV}" ]; then
 
     # ssh-add -l で登録されている鍵がなければ登録する
     if ! ssh-add -l >/dev/null 2>&1; then
+        echo "ssh-add -l で登録されている鍵がなければ登録する"
+        cat ~/.ssh/pass_phrase.txt
         ssh-add
     fi
 else
