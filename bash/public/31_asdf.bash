ebf0940ca82fdc37845b653b4f4059697f115a20
Seika139 <tcoanzu@gmail.com>
2025-08-24 03:55:42 +0900

    feat: asdfに関する設定を新規作成

---
+++ bash/public/31_asdf.bash
@@ -0,0 +1,16 @@
+#-------------------------------------
+# 12. asdf
+#     asdf とは「ツールのためのバージョンマネージャー」
+#     homebrew をもとに asdf を install / update する
+#     anyenv よりこっちの方がいいらしいので導入する
+#-------------------------------------
+
+# See: https://asdf-vm.com/ja-jp/guide/getting-started.html
+
+# echo 10
+# if executable brew; then
+#     echo 11111
+#     brew_upstall coreutils curl git asdf
+# fi
+
+# . "/opt/homebrew/opt/asdf/etc/bash_completion.d/asdf.bash"
