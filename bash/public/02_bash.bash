8e31fb662034f2427e6ec2e722aa624273aaefc6
Seika139 <tcoanzu@gmail.com>
2025-11-12 04:27:45 +0900

    bash-completion の読み込み方法を Homebrew のバージョンに変更

--- bash/public/02_bash.bash
+++ bash/public/02_bash.bash
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-# bash のコマンドの補完ツール(mac用)
-# 参考 : https://qiita.com/NorsteinBekkler/items/a0622ee6a39d08d61b72
+# Homebrew の bash-completion@2 を読み込む
 
-if executable brew; then
-    if [ -f "$(brew --prefix)/etc/bash_completion" ]; then
-        source "$(brew --prefix)/etc/bash_completion"
+completion_v2_path="/opt/homebrew/etc/profile.d/bash_completion.sh"
+if [[ -f "$completion_v2_path" ]]; then
+    source "$completion_v2_path"
+    # [DEBUG] bash-completion 読み込み直後に関数が存在するか確認
+    if type _comp_initialize &>/dev/null; then
+        echo "✅ _comp_initialize is found immediately after sourcing."
+    else
+        echo "❌ _comp_initialize is NOT found immediately after sourcing."
     fi
+else
+    warn "Homebrew の bash-completion@2 が見つかりません: $completion_v2_path"
 fi
