08aa34308e06d3ff0cddd7c1bb1425087ac76511
Seika139 <tcoanzu@gmail.com>
2025-11-12 04:28:24 +0900

    bash-completion v2 では _comp_initialize が使われないので、そもそもチェックする必要がない

--- bash/public/02_bash.bash
+++ bash/public/02_bash.bash
@@ -5,12 +5,6 @@
 completion_v2_path="/opt/homebrew/etc/profile.d/bash_completion.sh"
 if [[ -f "$completion_v2_path" ]]; then
     source "$completion_v2_path"
-    # [DEBUG] bash-completion 読み込み直後に関数が存在するか確認
-    if type _comp_initialize &>/dev/null; then
-        echo "✅ _comp_initialize is found immediately after sourcing."
-    else
-        echo "❌ _comp_initialize is NOT found immediately after sourcing."
-    fi
 else
     warn "Homebrew の bash-completion@2 が見つかりません: $completion_v2_path"
 fi
