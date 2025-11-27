d29e0453bbcfe4e1fe9c2206b15916fc35bd58bc
Seika139 <tcoanzu@gmail.com>
2025-11-28 02:07:43 +0900

    hlp_sed を追加した

--- bash/public/21_cheat_sheet.bash
+++ bash/public/21_cheat_sheet.bash
@@ -4,12 +4,12 @@
 
 # ファイルが存在する場合はその中身を出力する
 function cat_file() {
-    if [ -f $1 ]; then
+    if [ -f "$1" ]; then
         while IFS= read -r line; do
             # echo はダブルクォートで囲わないと連続するスペースが1つになってしまう
             # See : https://maku77.github.io/linux/io/echo-spaces.html
             echo -e "$line"
-        done <$1
+        done <"$1"
     else
         echo "No file exists with name of $1"
     fi
@@ -18,16 +18,16 @@ function cat_file() {
 # 文字に色をつけたファイルをlessで表示する
 # 一旦 cat_fileを挟まないとうまく色がつかないのでこうしている
 function less_color() {
-    cat_file $1 | less_lf
+    cat_file "$1" | less_lf
 }
 
 # 自作helpのトップ
 function hlp() {
-    less_lf ${DOTPATH}/docs/home.txt
+    less_lf "${DOTPATH}/docs/home.txt"
 }
 
 function hlp_alias() {
-    less_lf ${DOTPATH}/docs/linux/alias.txt
+    less_lf "${DOTPATH}/docs/linux/alias.txt"
 }
 
 function hlp_curl() {
@@ -35,31 +35,35 @@ function hlp_curl() {
 }
 
 function hlp_cursor() {
-    less_lf ${DOTPATH}/docs/linux/cursor.txt
+    less_lf "${DOTPATH}/docs/linux/cursor.txt"
 }
 
 function hlp_find() {
-    less_color ${DOTPATH}/docs/linux/find.txt
+    less_color "${DOTPATH}/docs/linux/find.txt"
 }
 
 function hlp_grep() {
-    less_color ${DOTPATH}/docs/linux/grep.txt
+    less_color "${DOTPATH}/docs/linux/grep.txt"
 }
 
 function hlp_history() {
-    less_lf ${DOTPATH}/docs/linux/history.txt
+    less_lf "${DOTPATH}/docs/linux/history.txt"
 }
 
 function hlp_less() {
-    less_lf ${DOTPATH}/docs/linux/less.txt
+    less_lf "${DOTPATH}/docs/linux/less.txt"
+}
+
+function hlp_sed() {
+    less_color "${DOTPATH}/docs/linux/sed.txt"
 }
 
 function hlp_wc() {
-    less_lf ${DOTPATH}/docs/linux/wc.txt
+    less_lf "${DOTPATH}/docs/linux/wc.txt"
 }
 
 function hlp_xargs() {
-    less_color ${DOTPATH}/docs/linux/xargs.txt
+    less_color "${DOTPATH}/docs/linux/xargs.txt"
 }
 
 #----------------------------------------------------------
