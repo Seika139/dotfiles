505daa846460a64267e2eb630f38b477c6a9661f
Seika139 <tcoanzu@gmail.com>
2026-01-21 02:14:03 +0900

    claude code の設定を更新

--- bash/envs/11_claude_envs.bash
+++ bash/envs/11_claude_envs.bash
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# Claude / Bedrock / OpenAI 関連のモデル指定は公開情報なのでここで定義する。
-export ANTHROPIC_MODEL='us.anthropic.claude-3-7-sonnet-20250219-v1:0'
-# export ANTHROPIC_MODEL='us.anthropic.claude-sonnet-4-20250514-v1:0'
-# export ANTHROPIC_MODEL='us.anthropic.claude-opus-4-20250514-v1:0'
-export ANTHROPIC_SMALL_FAST_MODEL='us.anthropic.claude-3-5-sonnet-20241022-v2:0'
-# export ANTHROPIC_SMALL_FAST_MODEL='us.anthropic.claude-3-5-haiku-20241022-v1:0'
+# Claude Code / OpenAI 関連のモデル指定は公開情報なのでここで定義する。
 export AWS_REGION='us-east-1'
 export CLAUDE_CODE_USE_BEDROCK=1
+export _DISABLE_ANTHROPIC_MODEL="global.anthropic.claude-opus-4-5-20251101-v1:0"
+export ANTHROPIC_MODEL="us.anthropic.claude-sonnet-4-5-20250929-v1:0"
+export ANTHROPIC_SMALL_FAST_MODEL="us.anthropic.claude-haiku-4-5-20251001-v1:0"
