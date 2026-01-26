81f77647cd3576d674817c3ffafab8e0b6d96185
Seika139 <tcoanzu@gmail.com>
2026-01-27 02:23:25 +0900

    謎のインテグラルを削除

--- bash/public/11_alias.bash
+++ bash/public/11_alias.bash
@@ -51,7 +51,7 @@ else
   alias egrep='egrep --color=auto'
 fi
 
-∫# cat を bat に置き換える
+# cat を bat に置き換える
 # 2025.09.03 追記: brewでインストールしたbatを使用
 if command -v bat &>/dev/null; then
   alias cat='bat --paging=never --style=plain'
