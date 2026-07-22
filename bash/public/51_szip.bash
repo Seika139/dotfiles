be6dfd9b640a5f9e1adee5a5be7a238f50b64ec9
Seika139 <tcoanzu@gmail.com>
2026-07-23 03:37:46 +0900

    `repo-preset` 関数を追加
    - 指定したディレクトリが Git リポジトリか確認する
    - リポジトリが有効な場合、設定を適用する

--- bash/public/51_szip.bash
+++ bash/public/51_szip.bash
@@ -7,3 +7,25 @@ suz() {
   tgt=$(abs_path "$*")
   (cd "$HOME/programs/tools/zipper/" && mise run decrypt "${tgt}")
 }
+
+repo-preset() {
+  # See: repo-preset/README.md
+  tgt=$(abs_path "$*")
+  local repo_root
+  if [ -d "$tgt" ]; then
+    if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+      repo_root=$(git rev-parse --show-toplevel)
+    else
+      echo_red -n "✗ Error: "
+      echo_yellow -n "'$tgt'"
+      echo " is not a git repository." >&2
+      return 1
+    fi
+  else
+    echo_red -n "✗ Error: "
+    echo_yellow -n "'$tgt'"
+    echo " is not a valid directory." >&2
+    return 1
+  fi
+  (cd "$DOTPATH" && mise trust -a && mise run repo-preset-install --target "${repo_root}")
+}
